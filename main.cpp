d0e7ede2a7ade0f45883357df1ae9a13dd896fd1
hsuanyulin <hsuanyulin.tw@gmail.com>
2018-05-07 18:37:22 +0200

    clean up

--- main.cpp
+++ main.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
-#include <cppunit/extensions/TestFactoryRegistry.h>
-#include <cppunit/TestResult.h>
-#include <cppunit/TestResultCollector.h>
-#include <cppunit/BriefTestProgressListener.h>
 #include <cppunit/ui/text/TestRunner.h>
-#include <cppunit/CompilerOutputter.h>
 #include <thread>
 #include <string>
 #include "LRUCache/LRUCache.h"
@@ -13,21 +8,9 @@
 using namespace std;
 
 int main() {
-  testSync();
-
   cout << "Testing cache functions.";
-  CPPUNIT_NS::TestResult testResult;
-  CPPUNIT_NS::TestResultCollector collectedResults;
-  CPPUNIT_NS::BriefTestProgressListener progress;
-  testResult.addListener (&collectedResults);
-  testResult.addListener (&progress);
   CPPUNIT_NS::TextUi::TestRunner runner;
   runner.addTest(LRUTestCase::suite());
 
-  bool isSuccessed = runner.run();
-
-  CPPUNIT_NS::CompilerOutputter output(&collectedResults, cerr);
-  output.write ();
-
-  return isSuccessed?0:1;
+  return runner.run() & testSync();
 }
